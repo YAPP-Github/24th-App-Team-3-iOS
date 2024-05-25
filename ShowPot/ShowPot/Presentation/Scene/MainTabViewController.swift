da297b66f8e59f17b87702a83c8297ee01dc6826
Daegun Choi  <chcs1370@gmail.com>
2024-05-25 19:14:38 +0900

    Re init

---
+++ ShowPot/ShowPot/Presentation/Scene/MainTabViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ShowPot
+//
+//  Created by Daegeon Choi on 5/25/24.
+//
+
+import UIKit
+
+class MainTabViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
