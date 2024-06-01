d0a1622a13097476585280d79491715d75a08652
dlrjswns <leekyunjun@gmail.com>
2024-06-02 02:18:00 +0900

    #10 스택 뷰 레이아웃 및 제약 추가

--- ShowPot/ShowPot/Presentation/Scene/Login/ViewController/LoginViewController.swift
+++ ShowPot/ShowPot/Presentation/Scene/Login/ViewController/LoginViewController.swift
@@ -5,17 +5,42 @@
 //  Created by Daegeon Choi on 6/1/24.
 //
 
-import Foundation
 import UIKit
 
-class LoginViewController: BaseViewController {
+import SnapKit
+import Then
+
+final class LoginViewController: BaseViewController {
     
     var coordinator: LoginCoordinator?
     
+    private let containerStackView = UIStackView().then {
+        $0.backgroundColor = .clear
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupLayouts()
+        setupConstraints()
+        setupStyles()
+        
         let item = UIBarButtonItem(title: "로그인", style: .plain, target: self, action: #selector(self.loginButtonDidTap))
         self.navigationItem.rightBarButtonItem = item
+    }
+    
+    private func setupLayouts() {
+        view.addSubview(containerStackView)
+    }
+    
+    private func setupConstraints() {
+        containerStackView.snp.makeConstraints {
+            $0.directionalEdges.equalToSuperview()
+        }
+    }
+    
+    private func setupStyles() {
+        
     }
     
     @objc
