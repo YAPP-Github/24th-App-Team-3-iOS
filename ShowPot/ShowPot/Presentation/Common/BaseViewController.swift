9b90703d501d19d812cee00ce4bc401d42a6d443
Daegun Choi  <chcs1370@gmail.com>
2024-06-11 20:04:54 +0900

    #15 View단에서 사용할 공용 프로토콜 개발
    - BaseViewControllerPorotocol, ViewModelType 추가
    - 해당 프로토콜 BaseViewController에 적용

--- ShowPot/ShowPot/Presentation/Common/BaseViewController.swift
+++ ShowPot/ShowPot/Presentation/Common/BaseViewController.swift
@@ -7,8 +7,47 @@
 
 import Foundation
 import UIKit
+import class RxSwift.DisposeBag
 
-// TODO: Need to work
-class BaseViewController: UIViewController {
+protocol BaseViewControllerPorotocol where Self: UIViewController {
+    associatedtype ViewHolder: ViewHolderType
+    associatedtype ViewModel: ViewModelType
     
+    var viewHolder: ViewHolder { get }
+    var viewModel: ViewModel { get }
 }
+
+extension BaseViewControllerPorotocol {
+    func viewHolderConfigure() {
+        viewHolder.place(in: view)
+        viewHolder.configureConstraints(for: view)
+    }
+}
+
+class BaseViewController: UIViewController  {
+    
+    /// A dispose bag. 각 ViewController에 종속적이다.
+    final let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO: 로그 추가
+        setupStyles()
+        bind()
+    }
+    
+    // 빈 영역 터치시 키보드 dismiss를 위한 코드
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    /// 현재 ViewController의 view 관련 설정 코드
+    ///
+    /// ***i.e.*** self.title = "title"
+    func setupStyles() { }
+    
+    /// ViewModel 바인딩에 필요한 코드
+    func bind() { }
+}
+
+typealias ViewController = BaseViewController & BaseViewControllerPorotocol
