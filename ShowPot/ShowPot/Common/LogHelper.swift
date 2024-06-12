2d4f183d804337be18713c3219f72c827bf11f69
dlrjswns <leekyunjun@gmail.com>
2024-06-12 21:23:53 +0900

    #20 LogHelper에서 사용할 함수 설명에 대한 주석 추가

--- ShowPot/ShowPot/Common/LogHelper.swift
+++ ShowPot/ShowPot/Common/LogHelper.swift
@@ -11,6 +11,16 @@ import os.log
 enum LogHelper {
     private static let subsystem = Bundle.main.bundleIdentifier!
     
+    /// Logger의 debug 레벨에 해당하는 로그를 작성할때 사용하는 함수입니다.
+    ///
+    /// 주로 디버깅에 유용한 정보를 표시하고싶을때 사용합니다.
+    ///
+    /// - Parameters
+    ///   - message: 로그에 담을 메세지
+    ///   - privacy: 디버깅이외에도 message 값을 그대로 보여줄지에 대한 타입
+    ///   - fileID: 다음 로그가 발생한 파일 아이디
+    ///   - line: 다음 로그가 발생한 줄
+    ///   - function: 다음 로그가 발생한 함수 이름
     static func debug(_ message: String, privacy: Privacy = .public, fileID: String = #fileID, line: Int = #line, function: String = #function) {
         let logger = Logger(subsystem: LogHelper.subsystem, category: Level.debug.category)
         let logMessage = "\(message)"
@@ -26,6 +36,16 @@ enum LogHelper {
         }
     }
     
+    /// Logger의 info 레벨에 해당하는 로그를 작성할때 사용하는 함수입니다.
+    ///
+    /// 오류 해결에 필수적이진 않지만 유용한 정보를 표시하고싶을때 사용합니다.
+    ///
+    /// - Parameters
+    ///   - message: 로그에 담을 메세지
+    ///   - privacy: 디버깅이외에도 message 값을 그대로 보여줄지에 대한 타입
+    ///   - fileID: 다음 로그가 발생한 파일 아이디
+    ///   - line: 다음 로그가 발생한 줄
+    ///   - function: 다음 로그가 발생한 함수 이름
     static func info(_ message: String, privacy: Privacy = .public, fileID: String = #fileID, line: Int = #line, function: String = #function) {
         let logger = Logger(subsystem: LogHelper.subsystem, category: Level.info.category)
         let logMessage = "\(message)"
@@ -41,6 +61,16 @@ enum LogHelper {
         }
     }
     
+    /// Logger의 error 레벨에 해당하는 로그를 작성할때 사용하는 함수입니다.
+    ///
+    /// 실행 중에 발생하는 오류를 표시하고싶을때 사용합니다.
+    ///
+    /// - Parameters
+    ///   - message: 로그에 담을 메세지
+    ///   - privacy: 디버깅이외에도 message 값을 그대로 보여줄지에 대한 타입
+    ///   - fileID: 다음 로그가 발생한 파일 아이디
+    ///   - line: 다음 로그가 발생한 줄
+    ///   - function: 다음 로그가 발생한 함수 이름
     static func error(_ message: String, privacy: Privacy = .public, fileID: String = #fileID, line: Int = #line, function: String = #function) {
         let logger = Logger(subsystem: LogHelper.subsystem, category: Level.error.category)
         let logMessage = "\(message)"
@@ -56,6 +86,16 @@ enum LogHelper {
         }
     }
     
+    /// Logger의 notice API를 사용하여 로그를 작성할때 사용하는 함수입니다.
+    ///
+    /// 문제 해결에 절대적으로 필요한 정보를 표시하고싶을때 사용합니다.
+    ///
+    /// - Parameters
+    ///   - message: 로그에 담을 메세지
+    ///   - privacy: 디버깅이외에도 message 값을 그대로 보여줄지에 대한 타입
+    ///   - fileID: 다음 로그가 발생한 파일 아이디
+    ///   - line: 다음 로그가 발생한 줄
+    ///   - function: 다음 로그가 발생한 함수 이름
     static func notice(_ message: String, privacy: Privacy = .public, fileID: String = #fileID, line: Int = #line, function: String = #function) {
         let logger = Logger(subsystem: LogHelper.subsystem, category: Level.notice.category)
         let logMessage = "\(message)"
