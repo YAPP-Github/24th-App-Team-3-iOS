4370aee2d51b960b478f1ede16056fd30b427078
dlrjswns <leekyunjun@gmail.com>
2024-06-12 19:00:52 +0900

    #20 Logger 생성할때 필요한 category에 대한 Level 열거형 타입 생성

--- ShowPot/ShowPot/Common/LogHelper.swift
+++ ShowPot/ShowPot/Common/LogHelper.swift
@@ -9,4 +9,31 @@ import Foundation
 
 struct LogHelper {
     
+extension LogHelper {
+    enum Level {
+        /// 디버깅 로그
+        case debug
+        
+        /// 오류 해결에 필수적이지않지만 유용한 정보 로그
+        case info
+        
+        /// 오류 로그
+        case error
+        
+        /// 문제 해결에 절대적으로 필요한 정보 로그
+        case notice
+        
+        fileprivate var category: String {
+            switch self {
+            case .debug:
+                return "🟡 DEBUG"
+            case .info:
+                return "🟠 INFO"
+            case .error:
+                return "🔴 ERROR"
+            case .notice:
+                return "🟢 NOTICE"
+            }
+        }
+    }
 }
