b1712195536bf949024e775edccd420c0b799beb
dlrjswns <leekyunjun@gmail.com>
2024-06-15 14:12:50 +0900

    #25 Pretendard & Oswald 폰트에 해당하는 String 열거형 코드 추가

--- ShowPot/ShowPot/Common/Extension/UIFont+Extension.swift
+++ ShowPot/ShowPot/Common/Extension/UIFont+Extension.swift
@@ -5,4 +5,48 @@
 //  Created by 이건준 on 6/15/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+  
+  enum Pretendard: String {
+    
+    // black
+    case black = "Pretendard-Black"
+    
+    // bold
+    case extraBold = "Pretendard-ExtraBold"
+    case bold = "Pretendard-Bold"
+    case semiBold = "Pretendard-SemiBold"
+    
+    // medium
+    case medium = "Pretendard-Medium"
+    
+    // regular
+    case regular = "Pretendard-Regular"
+    
+    // light
+    case light = "Pretendard-Light"
+    case extraLight = "Pretendard-ExtraLight"
+    
+    // thin
+    case thin = "Pretendard-Thin"
+  }
+  
+  enum Oswald: String {
+      
+      // light
+      case extraLight = "Oswald-ExtraLight"
+      case light = "Oswald-Light"
+      
+      // medium
+      case medium = "Oswald-Medium"
+      
+      // regular
+      case regular = "Oswald-Regular"
+      
+      // bold
+      case bold = "Oswald-Bold"
+      case semiBold = "Oswald-SemiBold"
+  }
+}
